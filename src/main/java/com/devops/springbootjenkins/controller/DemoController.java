01c46016f11f79593f56698968eaa2bd6ab242a9
ad-munir <mounir.ayad@etu.uae.ac.ma>
2023-11-05 23:58:13 +0100

    set /controller inside the /spbootjenkins package

--- src/main/java/com/devops/springbootjenkins/controller/DemoController.java
+++ src/main/java/com/devops/springbootjenkins/controller/DemoController.java
@@ -1,6 +1,7 @@
-package controller;
+package com.devops.springbootjenkins.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class DemoController {
