0d1f0e80ea8c58c03e63e66b68eac1c17c5d186a
ad-munir <mounir.ayad@etu.uae.ac.ma>
2023-11-05 22:29:03 +0100

    intial

---
+++ src/main/java/com/devops/springbootjenkins/SpringbootJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.devops.springbootjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/springbootjenkins/SpringbootJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.springbootjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
